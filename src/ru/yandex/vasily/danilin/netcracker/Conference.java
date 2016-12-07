b5a44e2a4b10e1a95a9b7130f55daf0106a98b47
DanVev <vasily.danilin@yandex.ru>
2016-12-07 14:55:26 +0300

    Change array to list in Conference

--- src/ru/yandex/vasily/danilin/netcracker/Conference.java
+++ src/ru/yandex/vasily/danilin/netcracker/Conference.java
@@ -1,9 +1,26 @@
 package ru.yandex.vasily.danilin.netcracker;
 
+import java.awt.*;
+import java.util.ArrayList;
+
 /***
  * Created by Vasily Danilin on 06.12.2016.
  */
 public class Conference {
-    Person[] People;
+    ArrayList<Person> people;
     String name;
+
+    public ArrayList<Person> getPeople() {
+        return people;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Conference(ArrayList<Person> people, String name) {
+
+        this.people = people;
+        this.name = name;
+    }
 }
